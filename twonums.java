4c35b40ee5f13fd7db3a7996137deb48b3095f46
Aaditya Hari Nair <aadityaharinair03@gmail.com>
2023-04-23 23:05:06 +0530

    Java folders

---
+++ twonums.java
@@ -0,0 +1,8 @@
+public class twonums {
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 24;
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
